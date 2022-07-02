3f720ca64709fdf728ef54bb158e9731bc82ea64
melt <meltryllis628@gmail.com>
2022-07-03 01:25:40 +0800

    feat: Complete Task 5

--- bin/source/count_line.c
+++ bin/source/count_line.c
@@ -26,7 +26,12 @@ int execute(char **args)
     // DO NOT PRINT ANYTHING TO THE OUTPUT
 
     /***** BEGIN ANSWER HERE *****/
-
+    size_t buf_size = SHELL_BUFFERSIZE;
+    char *buffer = malloc(sizeof(char) * buf_size);
+    while (getline(&buffer,&buf_size,fp)!=-1){
+      number_of_lines++;
+    }
+    free(buffer);
     /*********************/
     fclose(fp); // close file.
     printf("%d \t %s \n", number_of_lines, args[1]);
