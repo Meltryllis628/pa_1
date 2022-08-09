248bca95c9b6d8788d5acdd5e2099694d91df760
Meltryllis628 <56423470+Meltryllis628@users.noreply.github.com>
2022-08-10 06:00:48 +0800

    Add files via upload

---
+++ source/ClientWithSecurity.py
@@ -0,0 +1,185 @@
+from inspect import signature
+import pathlib
+import socket
+import sys
+import time
+from datetime import datetime
+import secrets
+import traceback
+
+from cryptography import x509
+from cryptography.exceptions import InvalidSignature
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
+
+def read_bytes(socket, length):
+    """
+    Reads the specified length of bytes from the given socket and returns a bytestring
+    """
+    buffer = []
+    bytes_received = 0
+    while bytes_received < length:
+        data = socket.recv(min(length - bytes_received, 1024))
+        if not data:
+            raise Exception("Socket connection broken")
+        buffer.append(data)
+        bytes_received += len(data)
+
+    return b"".join(buffer)
+
+def load_private_key(path):
+    with open(path,"rb")as file:
+        #certification = x509.load_pem_x509_certificate(file.read())
+        private_key = serialization.load_pem_private_key(file.read(),None)
+        return private_key
+
+def private_key(data):
+    private_key = serialization.load_pem_private_key(data,None)
+    return private_key
+
+def load_public_key(path):
+    with open(path,"rb")as file:
+        certification = x509.load_pem_x509_certificate(file.read())
+        public_key = certification.public_key()
+        t0 = time.time()
+        t2 = time.mktime(certification.not_valid_after.timetuple())
+        t1 = time.mktime(certification.not_valid_before.timetuple())
+        valid = (t0>t1)and(t0<t2)
+        return public_key,valid
+
+def public_key(data):
+    certification = x509.load_pem_x509_certificate(data)
+    public_key = certification.public_key()
+    t0 = time.time()
+    t2 = time.mktime(certification.not_valid_after.timetuple())
+    t1 = time.mktime(certification.not_valid_before.timetuple())
+    valid = (t0>t1)and(t0<t2)
+    return public_key,valid
+
+def sign(private_key,path):
+    with open(path,"rb")as file:
+        file_data=file.read()
+        signature = private_key.sign(
+            file_data,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH,
+            ),
+        hashes.SHA256(),  # Algorithm to hash the file_data before signing
+        )
+    return signature
+
+def vertify(signature,public_key,file_data):
+    try:
+        public_key.verify(
+            signature,
+            file_data,
+            padding.PSS( 
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH,
+            ),
+            hashes.SHA256(),
+        )
+        return True
+    except Exception as e:
+        return False
+
+def convert_int_to_bytes(x):
+    """
+    Convenience function to convert Python integers to a length-8 byte representation
+    """
+    return x.to_bytes(8, "big")
+
+
+def convert_bytes_to_int(xbytes):
+    """
+    Convenience function to convert byte value to integer value
+    """
+    return int.from_bytes(xbytes, "big")
+
+
+def main(args):
+    port = int(args[0]) if len(args) > 0 else 4321
+    server_address = args[1] if len(args) > 1 else "localhost"
+
+    start_time = time.time()
+
+    # try:
+    print("Establishing connection to server...")
+    # Connect to server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((server_address, port))
+        print("Connected")
+        arbitrary_message = b"assume this is an arbitraray message"
+        s.sendall(convert_int_to_bytes(3))
+        print("<-")
+        print(convert_int_to_bytes(3))
+        s.sendall(convert_int_to_bytes(len(arbitrary_message)))
+        print("<-")
+        print(convert_int_to_bytes(len(arbitrary_message)))
+        s.sendall(arbitrary_message)
+        print("<-")
+        print(arbitrary_message)
+        received_message1_length = convert_bytes_to_int(read_bytes(s,8))
+        encrypted_received_message1 = read_bytes(s,received_message1_length)
+        received_message2_length = convert_bytes_to_int(read_bytes(s,8))
+        received_message2 = read_bytes(s,received_message2_length)
+        pub,valid = public_key(received_message2)
+        if not valid:
+            s.sendall(convert_int_to_bytes(2))
+        signature = encrypted_received_message1
+        if not vertify(signature,pub,arbitrary_message):
+            s.sendall(convert_int_to_bytes(2))
+ 
+        while True:
+            # arbitrary_message = b"assume this is an arbitraray message"
+            # s.sendall(convert_int_to_bytes(3))
+            # s.sendall(convert_int_to_bytes(len(arbitrary_message)))
+            # s.sendall(arbitrary_message)
+            # received_message1_length = convert_bytes_to_int(read_bytes(s,8))
+            # encrypted_received_message1 = read_bytes(s,received_message1_length)
+            # received_message2_length = convert_bytes_to_int(read_bytes(s,8))
+            # received_message2 = read_bytes(s,received_message2_length)
+            # pub,valid = public_key(received_message2)
+            # if not valid:
+                # s.sendall(convert_int_to_bytes(2))
+            # signature = encrypted_received_message1
+            # if not vertify(signature,pub,arbitrary_message):
+                # s.sendall(convert_int_to_bytes(2))
+            filename = input(
+                "Enter a filename to send (enter -1 to exit):"
+            ).strip()
+
+            while filename != "-1" and (not pathlib.Path(filename).is_file()):
+                filename = input("Invalid filename. Please try again:").strip()
+
+            if filename == "-1":
+                s.sendall(convert_int_to_bytes(2))
+                break
+
+            filename_bytes = bytes(filename, encoding="utf8")
+
+            # Send the filename
+            s.sendall(convert_int_to_bytes(0))
+            s.sendall(convert_int_to_bytes(len(filename_bytes)))
+            s.sendall(filename_bytes)
+
+            # Send the file
+            with open(filename, mode="rb") as fp:
+                data = fp.read()
+                s.sendall(convert_int_to_bytes(1))
+                s.sendall(convert_int_to_bytes(len(data)))
+                s.sendall(data)
+
+        # Close the connection
+        s.sendall(convert_int_to_bytes(2))
+        print("Closing connection...")
+
+    end_time = time.time()
+    print(f"Program took {end_time - start_time}s to run.")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ source/ServerWithSecurity.py
@@ -0,0 +1,182 @@
+import pathlib
+import socket
+import sys
+import time
+from datetime import datetime
+import secrets
+import traceback
+
+from cryptography import x509
+from cryptography.exceptions import InvalidSignature
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
+
+def load_private_key(path):
+    with open(path,"rb")as file:
+        #certification = x509.load_pem_x509_certificate(file.read())
+        private_key = serialization.load_pem_private_key(file.read(),None)
+        return private_key
+
+def private_key(data):
+    private_key = serialization.load_pem_private_key(data,None)
+    return private_key
+
+def load_public_key(path):
+    with open(path,"rb")as file:
+        certification = x509.load_pem_x509_certificate(file.read())
+        public_key = certification.public_key()
+        t0 = time.time()
+        t2 = time.mktime(certification.not_valid_after.timetuple())
+        t1 = time.mktime(certification.not_valid_before.timetuple())
+        valid = (t0>t1)and(t0<t2)
+        return public_key,valid
+
+def public_key(data):
+    certification = x509.load_pem_x509_certificate(data)
+    public_key = certification.public_key()
+    t0 = time.time()
+    t2 = time.mktime(certification.not_valid_after.timetuple())
+    t1 = time.mktime(certification.not_valid_before.timetuple())
+    valid = (t0>t1)and(t0<t2)
+    return public_key,valid
+
+def sign(private_key,file_data):
+    signature = private_key.sign(
+        file_data,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH,
+        ),
+    hashes.SHA256(),  # Algorithm to hash the file_data before signing
+    )
+    return signature
+
+def vertify(signature,public_key,path):
+    with open(path,"rb")as file:
+        file_data=file.read()
+        try:
+            public_key.verify(
+                signature,
+                file_data,
+                padding.PSS( 
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH,
+                ),
+                hashes.SHA256(),
+            )
+            return True
+        except Exception as e:
+            return False
+
+def convert_int_to_bytes(x):
+    """
+    Convenience function to convert Python integers to a length-8 byte representation
+    """
+    return x.to_bytes(8, "big")
+
+
+def convert_bytes_to_int(xbytes):
+    """
+    Convenience function to convert byte value to integer value
+    """
+    return int.from_bytes(xbytes, "big")
+
+
+def read_bytes(socket, length):
+    """
+    Reads the specified length of bytes from the given socket and returns a bytestring
+    """
+    buffer = []
+    bytes_received = 0
+    while bytes_received < length:
+        data = socket.recv(min(length - bytes_received, 1024))
+        if not data:
+            raise Exception("Socket connection broken")
+        buffer.append(data)
+        bytes_received += len(data)
+
+    return b"".join(buffer)
+
+
+def main(args):
+    port = int(args[0]) if len(args) > 0 else 4321
+    address = args[1] if len(args) > 1 else "localhost"
+
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((address, port))
+            s.listen()
+
+            client_socket, client_address = s.accept()
+            with client_socket:
+                while True:
+                    match convert_bytes_to_int(read_bytes(client_socket, 8)):
+                        case 0:
+                            # If the packet is for transferring the filename
+                            print("Receiving file...")
+                            filename_len = convert_bytes_to_int(
+                                read_bytes(client_socket, 8)
+                            )
+                            filename = read_bytes(
+                                client_socket, filename_len
+                            ).decode("utf-8")
+                            # print(filename)
+                        case 1:
+                            # If the packet is for transferring a chunk of the file
+                            start_time = time.time()
+
+                            file_len = convert_bytes_to_int(
+                                read_bytes(client_socket, 8)
+                            )
+                            file_data = read_bytes(client_socket, file_len)
+                            # print(file_data)
+
+                            filename = "recv_" + filename.split("/")[-1]
+
+                            # Write the file with 'recv_' prefix
+                            with open(
+                                f"recv_files/{filename}", mode="wb"
+                            ) as fp:
+                                fp.write(file_data)
+                            print(
+                                f"Finished receiving file in {(time.time() - start_time)}s!"
+                            )
+                        case 2:
+                            # Close the connection
+                            # Python context used here so no need to explicitly close the socket
+                            print("Closing connection...")
+                            s.close()
+                            break
+                        case 3:
+                            file_len = convert_bytes_to_int(read_bytes(client_socket, 8)) 
+                            print("<-")
+                            print(file_len)
+                            file_data = read_bytes(client_socket, file_len)
+                            print("<-")
+                            print(file_data)
+                            pri = load_private_key("../source/auth/_private_key.pem")
+                            encrypted_file_data = sign(pri,file_data)
+                            client_socket.sendall(convert_int_to_bytes(len(encrypted_file_data)))
+                            print("->")
+                            print(len(encrypted_file_data))
+                            client_socket.sendall(encrypted_file_data)
+                            print("->")
+                            print(encrypted_file_data)
+                            with open("../source/auth/server_signed.crt","rb")as file:
+                                certification = file.read()
+                            client_socket.sendall(convert_int_to_bytes(len(certification)))
+                            print("->")
+                            print(len(certification))
+                            client_socket.sendall(certification)
+                            print("->")
+                            print(certification)
+
+    except Exception as e:
+        print(e)
+        s.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
